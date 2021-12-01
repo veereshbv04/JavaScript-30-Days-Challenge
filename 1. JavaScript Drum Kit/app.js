0c1e8f89e89e6033ef3ae4ef45b1f7528b80b1b9
Veeresh B V <Veereshbv04@gmail.com>
2021-12-01 23:50:21 +0530

    transition is not working

--- 1. JavaScript Drum Kit/app.js
+++ 1. JavaScript Drum Kit/app.js
@@ -1,9 +1,28 @@
 console.log("Hello World");
 
 function playSound(e){
-    
+ const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+ const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+ if(!audio) return;
+
+ key.classList.add("playing");
+
+ console.log(key);
+ audio.currentTime = 0;
+ audio.play();
+ 
 }
 
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove("playing");
+    console.log("hgg")
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+
+keys.forEach(key => key.addEventListener("transitioned", removeTransition));
 
 
 window.addEventListener("keydown", playSound);
